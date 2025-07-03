00b1473c9d94eb35ba37f47161e45d2a95d6c9f5
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2025-07-02 23:02:32 -0300

    Criando usuarios do modo editor

---
+++ BackEnd/models/database/config.php
@@ -0,0 +1,7 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "!@#$";
+    $banco = "db_blog_isadora_luparele";
+    $cmd = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+?>
